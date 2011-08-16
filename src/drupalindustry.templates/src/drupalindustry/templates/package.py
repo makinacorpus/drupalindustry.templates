b57bffb0ede3d8f4729eacab0b7771e580fe9783
Benoit Bryon <benoit.bryon@makina-corpus.com>
2011-08-16 08:45:32 +0200

    Introduced Drupal module template

--- src/drupalindustry.templates/src/drupalindustry/templates/package.py
+++ src/drupalindustry.templates/src/drupalindustry/templates/package.py
@@ -17,4 +17,24 @@ class DrupalBuildoutTemplate(MyTemplate):
             vars['with_drush'] = self.defaults['drush']
         self.boolify(vars)
 
+
+class DrupalModuleTemplate(MyTemplate):
+    """Template to generate a new Drupal module."""
+    _template_dir = 'tmpl/module'
+    summary = "A blank Drupal module."
+    defaults = getdefaults('module')
+
+    def __init__(self, name):
+        super(DrupalModuleTemplate, self).__init__(name)
+        self.vars = [
+            var('name', 'Human-readabale name'),
+            var('description', 'Description of the module'),
+            var('core_version', 'Core version', default=self.defaults['core_version']),
+            var('version', 'Version of the module', default=self.defaults['version']),
+            var('package', 'Package', default=self.defaults['package']),
+        ]
+
+    def pre(self, command, output_dir, vars):
+        self.boolify(vars)
+
 # vim:set et sts=4 ts=4 tw=80:
